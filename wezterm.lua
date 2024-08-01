7e0637fd44cedf62b8d2ba8cc4317e5cf0b4fde3
god464 <god464@users.noreply.github.com>
2024-08-01 20:45:54 +0800

    rollback to tokyonight

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,7 @@
 local config = require("wezterm").config_builder()
 config = {
   font = require("fonts").set_font(),
-  color_scheme = "Nord",
+  color_scheme = "Tokyo Night Storm",
   enable_scroll_bar = true,
   hide_tab_bar_if_only_one_tab = true,
   use_ime = true,
