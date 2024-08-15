50359792f7e75f3f7cf8254c085e4c3821fea800
god464 <god464@users.noreply.github.com>
2024-08-15 11:39:50 +0800

    using catppuccin instead of tokyonight

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,7 @@
 local config = require("wezterm").config_builder()
 config = {
   font = require("fonts").set_font(),
-  color_scheme = "Tokyo Night Storm",
+  color_scheme = "Catppuccin Mocha",
   enable_scroll_bar = true,
   hide_tab_bar_if_only_one_tab = true,
   use_ime = true,
