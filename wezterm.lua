8794ba01137e54941a7cb4be08e4b178e6ad6029
god464 <god464@users.noreply.github.com>
2025-01-29 15:34:08 +0800

    refactor: use gruvbox dark hard

--- wezterm.lua
+++ wezterm.lua
@@ -11,7 +11,7 @@ end
 
 config = {
   font = require("fonts"),
-  color_scheme = "Tokyo Night Storm",
+  color_scheme = "GruvboxDarkHard",
   enable_scroll_bar = true,
   hide_tab_bar_if_only_one_tab = true,
   use_ime = true,
